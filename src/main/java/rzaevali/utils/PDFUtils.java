3eb0395ac687a27dde2e75e3a8797a5036c86814
Ali Rzaev <alirzaev1996@gmail.com>
2017-10-13 15:07:59 +0300

    Switch to new version of tabula-java

--- src/main/java/rzaevali/utils/PDFUtils.java
+++ src/main/java/rzaevali/utils/PDFUtils.java
@@ -11,21 +11,32 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PDFUtils {
-    private static final String BASE_URL = "https://www.vyatsu.ru/reports/schedule/Group/%s_1_09102017_22102017.pdf";
+import rzaevali.exceptions.DocNotFoundException;
 
-    private static class Entity {
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class PDFUtils {
+    private static final String BASE_URL = "https://www.vyatsu.ru/reports/schedule/Group/%s_1_%s_%s.pdf";
+
+    private static class Schedule {
         private List<List<List<String>>> weeks;
         private String group;
 
-        Entity(List<List<List<String>>> weeks, String group) {
+        Schedule(List<List<List<String>>> weeks, String group) {
             this.weeks = weeks;
             this.group = group;
         }
     }
 
-    public static String parseSchedule(String groupId) throws IOException {
-        URL url = new URL(String.format(BASE_URL, groupId));
+    public static String parseSchedule(String groupId, String season) throws IOException,
+                                                                             DocNotFoundException {
+        checkNotNull(groupId, "groupId must not be null");
+        checkNotNull(season, "season must not be null");
+
+        DBUtils.DateRange range = DBUtils.getDateRange(groupId, season);
+
+        URL url = new URL(String.format(BASE_URL, groupId, range.getFirst(), range.getSecond()));
         List<String> rows = parsePDFFile(url);
         List<List<String>> days = new ArrayList<>();
         for (int i = 0; i < 14; ++i) {
@@ -47,11 +58,11 @@ public class PDFUtils {
         return new GsonBuilder().
                 setPrettyPrinting().
                 create().
-                toJson(new Entity(weeks, group));
+                toJson(new Schedule(weeks, group));
     }
 
     private static List<String> parsePDFFile(URL url) throws IOException {
-        PDDocument pdfDocument = PDDocument.load(url);
+        PDDocument pdfDocument = PDDocument.load(url.openStream());
         PageIterator pageIterator = new ObjectExtractor(pdfDocument).extract();
         ExtractionAlgorithm algorithm = new SpreadsheetExtractionAlgorithm();
         List<Table> tables = new ArrayList<>();
