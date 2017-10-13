c30b507be518478e803725baacfbca34d82b11c7
Ali Rzaev <alirzaev1996@gmail.com>
2017-10-13 15:11:38 +0300

    Enable db updater

--- src/main/java/rzaevali/updater/Main.java
+++ src/main/java/rzaevali/updater/Main.java
@@ -4,7 +4,7 @@ import rzaevali.utils.DBUtils;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        DBUtils.updateDateRanges("autumn");
-        System.out.println(DBUtils.getDateRange("7794", "autumn").toString());
+       DBUtils.updateDateRanges("autumn");
+       // DBUtils.updateDateRanges("spring");
     }
 }
