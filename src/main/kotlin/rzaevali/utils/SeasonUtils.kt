156c65f868a9a4fe1b9211107179f39c18cffd90
Али <alirzaev1996@gmail.com>
2018-02-16 14:01:18 +0300

    Add docs for SeasonUtils

--- src/main/kotlin/rzaevali/utils/SeasonUtils.kt
+++ src/main/kotlin/rzaevali/utils/SeasonUtils.kt
@@ -6,6 +6,14 @@ const val SEASON_AUTUMN = "autumn"
 
 const val SEASON_SPRING = "spring"
 
+/**
+ * Check whether the season name is valid or not and return it
+ * The valid names are [SEASON_AUTUMN] and [SEASON_SPRING]
+ *
+ * @param season season name
+ * @return season name if it is valid
+ * @throws UnknownValueException if season name doesn't equal to [SEASON_AUTUMN] or [SEASON_SPRING]
+ */
 @Throws(UnknownValueException::class)
 fun checkSeason(season: String): String {
     return if (season != SEASON_AUTUMN && season != SEASON_SPRING) {
@@ -15,6 +23,13 @@ fun checkSeason(season: String): String {
     }
 }
 
+/**
+ * Map season name to the season key
+ *
+ * @param season season name
+ * @return season key
+ * @throws UnknownValueException if season name isn't valid
+ */
 @Throws(UnknownValueException::class)
 fun getSeasonKey(season: String): String {
     checkSeason(season)
