2b85862626f44083f768cf7f35be73fe127862d4
Ali Rzaev <alirzaev1996@gmail.com>
2018-06-09 16:19:21 +0300

    Add module for accessing schedule ranges

---
+++ models/schedule_ranges.py
@@ -0,0 +1,31 @@
+from utils.db_config import get_collection
+from typing import Optional, List
+from pymongo import ReplaceOne
+
+_collection = get_collection('schedule_ranges')
+
+
+def find_by_group_and_season(group_id: str, season: str) -> Optional[dict]:
+    document = _collection.find_one(
+        filter={
+            'groupId': group_id,
+            'season': season
+        }
+    )
+
+    return document
+
+
+def upsert_documents(documents: List[dict]):
+    operations = [
+        ReplaceOne(
+            filter={
+                'groupId': document['groupId'],
+                'season': document['season']
+            },
+            replacement=document,
+            upsert=True
+        ) for document in documents
+    ]
+
+    _collection.bulk_write(operations, ordered=False)
