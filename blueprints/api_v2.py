493e8feb7780a094f5970fa659484dbeb3c210ec
Ali Rzaev <alirzaev1996@gmail.com>
2018-06-10 12:47:17 +0300

    Implement API /api/v2/calls

--- blueprints/api_v2.py
+++ blueprints/api_v2.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 from utils.wrappers import on_exception, content_type_json
 
-
 api_v2_blueprint = Blueprint('api_v2', __name__)
 
 
@@ -29,9 +28,15 @@ def get_groups_by_faculty():
 @on_exception(500)
 @content_type_json
 def get_calls():
-    return {
-        'meta': '/calls'
-    }
+    return [
+        {"start": "8:20", "end": "9:50"},
+        {"start": "10:00", "end": "11:30"},
+        {"start": "11:45", "end": "13:15"},
+        {"start": "14:00", "end": "15:30"},
+        {"start": "15:45", "end": "17:15"},
+        {"start": "17:20", "end": "18:50"},
+        {"start": "18:55", "end": "20:25"}
+    ]
 
 
 @api_v2_blueprint.route('/schedule/<group_id>/<season>', methods=['GET'])
