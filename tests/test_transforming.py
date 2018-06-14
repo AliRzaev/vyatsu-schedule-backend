623f233bfbbadeca94727e858945563a05bf7b65
Ali Rzaev <alirzaev1996@gmail.com>
2018-06-14 14:40:15 +0300

    Remove DB dependency

--- tests/test_transforming.py
+++ tests/test_transforming.py
@@ -1,8 +1,5 @@
-from unittest import TestCase
-
-from models import groups_info
-
 from json import load
+from unittest import TestCase
 
 from utils.transforming import api_v1, api_v2
 
@@ -10,21 +7,19 @@ from utils.transforming import api_v1, api_v2
 class TestTransformingV1(TestCase):
 
     def setUp(self):
-        groups_info.delete_all()
-
         with open('tests/resources/test_groups_data.json', 'r', encoding='utf-8') as file:
             data = load(file)
 
-        groups_info.upsert_documents([
+        self.data = [
             {
                 'groupId': item['id'],
                 'group': item['name'],
                 'faculty': item['faculty']
             } for item in data
-        ])
+        ]
 
     def test_groups_list(self):
-        data = api_v1.groups_info_to_dict(groups_info.find_all())
+        data = api_v1.groups_info_to_dict(self.data)
 
         with open('tests/resources/v1/test_groups_list.json', 'r', encoding='utf-8') as file:
             expected_data = load(file)
@@ -32,7 +27,7 @@ class TestTransformingV1(TestCase):
         self.assertEqual(data, expected_data, 'Invalid transformation')
 
     def test_groups_by_faculty(self):
-        data = api_v1.groups_info_to_dict(groups_info.find_all(), True)
+        data = api_v1.groups_info_to_dict(self.data, True)
 
         with open('tests/resources/v1/test_groups_by_faculty.json', 'r', encoding='utf-8') as file:
             expected_data = load(file)
@@ -43,21 +38,19 @@ class TestTransformingV1(TestCase):
 class TestTransformingV2(TestCase):
 
     def setUp(self):
-        groups_info.delete_all()
-
         with open('tests/resources/test_groups_data.json', 'r', encoding='utf-8') as file:
             data = load(file)
 
-        groups_info.upsert_documents([
+        self.data = [
             {
                 'groupId': item['id'],
                 'group': item['name'],
                 'faculty': item['faculty']
             } for item in data
-        ])
+        ]
 
     def test_groups_list(self):
-        data = sorted(api_v2.groups_info_to_list(groups_info.find_all()), key=lambda x: x['id'])
+        data = sorted(api_v2.groups_info_to_list(self.data), key=lambda x: x['id'])
 
         with open('tests/resources/v2/test_groups_list.json', 'r', encoding='utf-8') as file:
             expected_data = load(file)
@@ -71,7 +64,7 @@ class TestTransformingV2(TestCase):
                 item['groups'].sort(key=lambda x: x['id'])
             data.sort(key=lambda x: x['faculty'])
 
-        data = api_v2.groups_info_to_list(groups_info.find_all(), True)
+        data = api_v2.groups_info_to_list(self.data, True)
         sort_data(data)
 
         with open('tests/resources/v2/test_groups_by_faculty.json', 'r', encoding='utf-8') as file:
