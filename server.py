fe6dd49de0fc9daf031d98ffca9514b9f4aaff9f
Ali Rzaev <alirzaev1996@gmail.com>
2018-06-14 14:43:08 +0300

    Code refactoring

--- server.py
+++ server.py
@@ -3,21 +3,12 @@ from os import getenv
 from flask import Flask
 from flask_cors import CORS
 
-from utils.wrappers import on_exception
 from blueprints.api_v1 import api_v1_blueprint
 from blueprints.api_v2 import api_v2_blueprint
 
 PORT = getenv('PORT', '8080')
-IP = getenv('IP', '0.0.0.0')
 
 app = Flask(__name__)
-
-
-# For testing
-@app.route('/test', methods=['GET'])
-@on_exception(500)
-def get_test():
-    return 'OK'
 
 
 CORS(api_v1_blueprint, methods=['GET'])
@@ -31,4 +22,4 @@ app.register_blueprint(api_v2_blueprint, url_prefix='/api/v2')
 
 
 if __name__ == '__main__':
-    app.run(IP, PORT, debug=True)
+    app.run(port=PORT, debug=True)
