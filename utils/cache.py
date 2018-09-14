cf2a86deb1737504c860db4226383a4d2782f29c
Ali Rzaev <alirzaev1996@gmail.com>
2018-09-14 16:36:30 +0300

    Move database specific code into separate class
    It should improve unit test writing

--- utils/cache.py
+++ utils/cache.py
@@ -1,20 +1,53 @@
 from typing import Any
 
-from pymongo.collection import Collection
+from pymongo.collection import Collection as MongoCollection
+
+
+class CollectionAdapter:
+
+    def find_one(self, key: str) -> Any:
+        pass
+
+    def update_one(self, key: str, value: Any):
+        pass
+
+    def delete_one(self, key: str):
+        pass
+
+
+class MongoCollectionAdapter(CollectionAdapter):
+
+    def __init__(self, collection: MongoCollection):
+        self._collection = collection
+
+    def find_one(self, key: str) -> Any:
+        return self._collection.find_one({
+            'key': key
+        })
+
+    def update_one(self, key: str, value: Any):
+        self._collection.update_one(
+            filter={'key': key},
+            update={'$set': {'key': key, 'value': value}},
+            upsert=True
+        )
+
+    def delete_one(self, key: str):
+        self._collection.delete_one({
+            'key': key
+        })
 
 
 class KeyValueStorage:
 
-    def __init__(self, collection: Collection):
+    def __init__(self, collection: CollectionAdapter):
         self._collection = collection
 
     def __getitem__(self, key: str) -> Any:
         if not isinstance(key, str):
             raise TypeError('Key must be a string')
 
-        document = self._collection.find_one({
-            'key': key
-        })
+        document = self._collection.find_one(key)
 
         if document is None:
             raise KeyError(key)
@@ -25,16 +58,10 @@ class KeyValueStorage:
         if not isinstance(key, str):
             raise TypeError('Key must be a string')
 
-        self._collection.update_one(
-            filter={'key': key},
-            update={'$set': {'key': key, 'value': value}},
-            upsert=True
-        )
+        self._collection.update_one(key, value)
 
     def __delitem__(self, key: str):
         if not isinstance(key, str):
             raise TypeError('Key must be a string')
 
-        self._collection.delete_one({
-            'key': key
-        })
+        self._collection.delete_one(key)
