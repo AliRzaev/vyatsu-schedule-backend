4c11908dec4fafb46071dff9ac4ce76fd5b95151
AliRzaev <alirzaev1996@gmail.com>
2018-12-06 20:44:44 +0300

    Enable logging to console in production environment

--- server.py
+++ server.py
@@ -7,10 +7,12 @@ from blueprints.api_v1 import api_v1_blueprint
 from blueprints.api_v2 import api_v2_blueprint
 
 from models import logs
+from utils.logging import get_logger
 
 PORT = getenv('PORT', '8080')
 
 app = Flask(__name__)
+logger = get_logger(__name__)
 
 
 CORS(api_v1_blueprint, methods=['GET'])
@@ -26,6 +28,12 @@ app.register_blueprint(api_v2_blueprint, url_prefix='/api/v2')
 @app.before_request
 def logs_to_mongo():  # logs each request into MongoDB
     logs.insert_one(request.full_path)
+
+
+if __name__ != '__main__':  # logs to console in production environment
+    @app.before_request
+    def logs_to_console():
+        logger.info('{} {}'.format(request.method, request.full_path))
 
 
 @app.after_request
