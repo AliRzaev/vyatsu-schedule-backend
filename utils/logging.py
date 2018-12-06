3d6970e73f234ad0b26cfe23f3d6fb62bb50292e
AliRzaev <alirzaev1996@gmail.com>
2018-12-06 20:45:17 +0300

    Lead log record format to the standard gunicorn format

--- utils/logging.py
+++ utils/logging.py
@@ -5,7 +5,10 @@ def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        '[%(asctime)s] [%(process)d] [%(levelname)s] [%(name)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S %z'
+    )
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
