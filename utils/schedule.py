b93d41482d78beb1945e190970c8438bd17e7360
AliRzaev <alirzaev1996@gmail.com>
2018-12-28 12:30:09 +0300

    Use functools.lru_cache in fetch_schedule function
    lru_cache is activated with parameter maxsize=512 to remain
    performance at the same level.
    
    As we apply lru_cache decorator directly to fetch_schedule function
    there is no need in fetch_schedule_from_service function.

--- utils/schedule.py
+++ utils/schedule.py
@@ -1,29 +1,24 @@
-from datetime import timedelta
+from functools import lru_cache
 from os import getenv
-from typing import List
 
 import requests
 
-from utils.cache import KeyValueStorage, RedisCollectionAdapter
-from config.redis import get_instance
-
-CACHE_PREFIX = __name__
+from utils.groups_info import DateRange
 
 _PARSE_API_URL = getenv('PARSE_API_URL')
 
-_SCHEDULE_URL_TEMPLATE = 'https://www.vyatsu.ru/reports/schedule/Group/{}_{}_{}_{}.pdf'
-
-_SCHEDULE_CACHE = KeyValueStorage(
-    RedisCollectionAdapter(get_instance(), timedelta(days=14)))
+_URL_TEMPLATE = 'https://www.vyatsu.ru/reports/schedule/Group/{}_{}_{}_{}.pdf'
 
 
 class ParseException(Exception):
     pass
 
 
-def fetch_schedule_from_service(group_id: str, season_key: str, _range: List[str]) -> dict:
+@lru_cache(maxsize=512)
+def fetch_schedule(group_id: str, season_key: str, range_: DateRange):
     api_url = _PARSE_API_URL + '/api/v1/parse_pdf'
-    pdf_url = _SCHEDULE_URL_TEMPLATE.format(group_id, season_key, _range[0], _range[1])
+    pdf_url = _URL_TEMPLATE.format(group_id, season_key,
+                                   range_[0], range_[1])
 
     response = requests.get(api_url, params={'url': pdf_url})
     response.raise_for_status()
@@ -36,22 +31,3 @@ def fetch_schedule_from_service(group_id: str, season_key: str, _range: List[str
         raise ParseException(data['meta']['error'])
     else:
         raise ParseException('PDF_PARSE_ERROR')
-
-
-def fetch_schedule(group_id: str, season_key: str, _range: List[str]) -> dict:
-    schedule_key = f'{CACHE_PREFIX}_schedule_{group_id}_{season_key}'
-    try:
-        cached_schedule = _SCHEDULE_CACHE[schedule_key]
-        if cached_schedule['range'] != _range:
-            raise Exception('Outdated schedule')
-        else:
-            return cached_schedule['schedule']
-    except (KeyError, Exception):
-        actual_schedule = fetch_schedule_from_service(group_id, season_key, _range)
-
-        _SCHEDULE_CACHE[schedule_key] = {
-            'schedule': actual_schedule,
-            'range': _range
-        }
-
-        return actual_schedule
