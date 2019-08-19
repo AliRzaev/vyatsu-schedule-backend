9baaa8ad31b5bb221aa90ba79be3c1f897649d99
AliRzaev <alirzaev1996@gmail.com>
2019-08-19 20:03:50 +0300

    Restructure tests
    'test_groups_info.py' and 'test_departments_info.py' was merged into 
    'test_repository.py' since they test the functionality of 
    'utils.repository' module.

--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -1,7 +1,7 @@
 from utils.repository import get_repository
 
 
-class TestDepartmentsInfo:
+class TestRepository:
     """
     Be attentive! This test case may wipe out data in your database.
     Please ensure that you run test case with database for testing,
@@ -15,5 +15,14 @@ class TestDepartmentsInfo:
 
             actual = sorted(get_repository().get_departments())
             expected = departments_info
+
+            assert actual == expected, 'Invalid data'
+
+    def test_get_groups(self, flask_app, groups_info, prefetch_groups):
+        with flask_app.app_context():
+            prefetch_groups()
+
+            actual = sorted(get_repository().get_groups())
+            expected = groups_info
 
             assert actual == expected, 'Invalid data'
